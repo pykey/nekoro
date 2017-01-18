76a06b6c83b4c5835e1dfa6d0ff5c68d6037ab14
Iker Pérez Brunelli <yo@ikerperez.es>
2017-01-18 20:47:04 +0100

    Add Parse server, removed cors and added nodemon for development

--- server/middlewares/frontendMiddleware.js
+++ server/middlewares/frontendMiddleware.js
@@ -1,17 +1,34 @@
+/** @module middleware/frontend */
+
 const express = require('express');
-const compression = require('compression');
-const cors = require('cors');
-const helmet = require('helmet');
 const path = require('path');
 
+/**
+ * Frontend middleware
+ *
+ * @param {express} app Express app instance
+ * @param {Object} options Options to load the frontend
+ * @returns {express} Express app instance
+ */
 module.exports = (app, options = {}) => {
+  /**
+   * Base path where the application runs
+   *
+   * @type {string}
+   * @default /
+   */
   const publicPath = options.publicPath || '/';
+
+  /**
+   * Path where are the static files
+   *
+   * @type {string}
+   */
   const outputPath = options.outputPath || path.resolve(process.cwd(), 'dist');
 
-  app.use(compression());
-  app.use(cors());
-  app.use(helmet());
   app.use(publicPath, express.static(outputPath));
 
   app.get('*', (req, res) => res.sendFile(path.resolve(outputPath, 'index.html')));
+
+  return app;
 };
