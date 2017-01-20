af9d59a419f3817d022863a1daf4aa7143ea7df9
Iker Pérez Brunelli <yo@ikerperez.es>
2017-01-20 03:57:30 +0100

    Use Bolt to configure database rules, configure travis to not deploy on pull requests and fix error on production environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  debug: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC2c9BE5LXSEBZjlqmqsBCyIbHvMqFbkNs',
+    authDomain: 'nekoro-c5e01.firebaseapp.com',
+    databaseURL: 'https://nekoro-c5e01.firebaseio.com',
+    storageBucket: 'nekoro-c5e01.appspot.com',
+    messagingSenderId: '688361697578'
+  }
 };
